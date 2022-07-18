ce71ce97d5eb7caad771728153fa010b6cb82d07
Mariotin <gmariotin@gmail.com>
2022-07-18 20:01:25 -0300

    segunda.2

--- pokedex4/src/pages/Home.js
+++ pokedex4/src/pages/Home.js
@@ -1,11 +1,39 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import logo from "../img/logo.jpeg"
 import {gotoPokedexPage} from "../routes/Coordinator"
 import "./Home.css"
+import axios from "axios"
 
 export const Home = () => {
     const navigate = useNavigate()  
+    
+  const [pokeData, setPokeData] = useState([])
+  const [pokeImg, setPokeImg] = useState()
+
+  const Named = () =>{
+    const url = "https://pokeapi.co/api/v2/pokemon/"
+    axios.get(url)
+    .then((response) =>{
+      setPokeData(response.data.results)
+      setPokeImg(response.sprites.front_default)
+      console.log(response.data.results)
+    })
+    .catch((erro) =>{
+      console.log(erro)
+    })
+  }
+
+  useEffect(() =>{
+    Named()
+  },[])
+  
+  const pokeMap = pokeData.map((pokemon) =>{
+    return (
+      <p key={pokemon.name}> {pokemon.name}  </p>
+    )
+  })
+
        
     return (
         <div>
@@ -16,6 +44,8 @@ export const Home = () => {
             </div>
             <div className="ContainerMain">
             <h1 className="Paragrafo">Todos os Pokémons</h1>
+            {pokeMap} 
+            {/* <img src={pokeImg} /> */}
             </div>
         </div>
     )
