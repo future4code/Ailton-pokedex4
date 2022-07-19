cdc0550f23d38709e1b97e2bf7462b6427966d64
Leonardo Simas <leonardosimasrodrigues@gmail.com>
2022-07-19 17:10:55 -0300

    Segundo dia do Pokedex

--- pokedex4/src/components/CardPokedex.js
+++ pokedex4/src/components/CardPokedex.js
@@ -1,12 +1,19 @@
 import React from "react"
+import "./CardPokedex.css"
 
 
-export const CardPokedex = () => {
+export const CardPokedex = (props) => {
+    const pokemon = props.pokemon
     return (
         <div>
-            <div className="CardViagens">
-                <p className="Margem"><strong>Nome:</strong> &nbsp;{props.aprovado.name}</p>
-                <p className="Margem"><strong>Idade:</strong> &nbsp;{props.aprovado.age} &nbsp; dias</p>
+            <div className="Container">
+                <p>{pokemon.name}-{pokemon.id}</p>
+                <img src={pokemon.sprites.front_default} />
+                <p>{pokemon.types.name}</p>
+                {/* <div>
+                <button>Detalhes</button>
+                <button>Capturar</button>
+                </div> */}
             </div>
         </div>
     )
