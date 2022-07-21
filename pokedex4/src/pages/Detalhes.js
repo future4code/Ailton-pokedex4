542ca094e96716d72901f47aad3198758449d290
Mariotin <gmariotin@gmail.com>
2022-07-21 11:25:57 -0300

    atualizado

--- pokedex4/src/pages/Detalhes.js
+++ pokedex4/src/pages/Detalhes.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState }  from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { gotoHomePage } from "../routes/Coordinator"
 import { useNavigate } from "react-router-dom"
 import "./Detalhes.css"
@@ -8,40 +8,31 @@ import { useParams } from "react-router-dom"
 import { CardDetalhes } from "../components/CardDetalhes"
 import axios from "axios"
 
-export const Detalhes = () => {
+  export const Detalhes = () => {
+
   const navigate = useNavigate()
-  const  {states} = useContext(GlobalContext)
-  const {pokemonDetails, pokeData} = states
+  const { states } = useContext(GlobalContext)
+  const { pokemonDetails, pokeData } = states
   const params = useParams()
-
   const [detalhe, setDetalhe] = useState([])
-
-  useEffect(() =>{
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${params.id}/`)
-    .then((res) =>{
-      setDetalhe(res.data)
-    })
-    .catch((erro) =>{
-      console.log(erro)
-    })
-  }, [])
+  
+  const juntaDados = pokemonDetails[params.id-1]
 
   console.log(detalhe)
- 
-    return (
-      <div>
+
+  return (
+    <div>
       <div className="CountainerHeader">
-      <button onClick={() => gotoHomePage(navigate)} className="Botao">Todos os Pokémons</button>
+        <button onClick={() => gotoHomePage(navigate)} className="Botao">Todos os Pokémons</button>
         <img src={logo} className="Logo" />
         <button>Excluir</button>
       </div>
       <div className="ContainerMain">
         <h1 className="Paragrafo">Detalhes</h1>
       </div>
-      <div>
-      <p>{detalhe.name}</p>
-      {/* <CardDetalhes detalhe={detalhe}/> */}
+      <div className="ContainerMain2">
+         <CardDetalhes detalhe={juntaDados} />
       </div>
     </div>
-    )
+  )
 }
